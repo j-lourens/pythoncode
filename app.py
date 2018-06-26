e5ab344bab47891d3c2538bb4b7fdc4dcbe78ff3
j-lourens <40594381+j-lourens@users.noreply.github.com>
2018-06-26 11:56:47 +0200

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ def rest_request_example():
 
 def read_db_SQL_example():
     conn = db.get_engine().connect()
-    sql = "SELECT * FROM SampleTable"
+    sql = "SELECT * FROM SecretTable"
     results = conn.execute(sql)
     rows = ""
     for row in results:
@@ -29,7 +29,6 @@ def read_db_SQL_example():
 rest_request_example()
 try:
 	read_db_SQL_example()
-	rest_request_example()
 except:
 	print ('Could not connect to database')
 	
