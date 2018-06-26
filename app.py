dd3aebf20c4271a706db3c786504b438513c5d9c
j-lourens <40594381+j-lourens@users.noreply.github.com>
2018-06-26 12:22:57 +0200

    Update app.py

--- app.py
+++ app.py
@@ -14,9 +14,7 @@ def home():
      # My microservice!
     return render_template('home.html')
 
-
 def rest_request_example():
-    
     print (requests.get("http://140.86.15.104:3000/reactorCore/320/650/yellow/j-lourens").text) 
 
 def read_db_SQL_example():
