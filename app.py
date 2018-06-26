7a413ae8fe36854db71c97dac28d7c6ce4bbf206
j-lourens <40594381+j-lourens@users.noreply.github.com>
2018-06-26 12:24:45 +0200

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def home():
     return render_template('home.html')
 
 def rest_request_example():
-    print (requests.get("http://140.86.15.104:3000/reactorCore/320/650/yellow/j-lourens").text) 
+    print (requests.get("http://140.86.15.104:3000/reactorCore/320/650/yellow/jlourens").text) 
 
 def read_db_SQL_example():
     conn = db.get_engine().connect()
